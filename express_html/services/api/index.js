74261a156a693f11aa4abe2db74940f8883ec69f
fuyanran520 <fuyanran@dwnews.com>
2019-06-06 17:28:41 +0800

    docs: 查询样式

--- express_html/services/api/index.js
+++ express_html/services/api/index.js
@@ -1,14 +1,13 @@
 import { platform } from "os";
 
 global.fetch = require("node-fetch");
-const devURL = process.env.globalUrl;
-console.log(devURL,'devURL-----')
+//const devURL = process.env.globalUrl;
+const devURL = "http://localhost:8001";
+console.log(devURL, "devURL11-----");
 
 export const getUserList = async params => {
   const res = await fetch(
-    `${devURL}/user/userList?page=${params.page}&pageSize=${
-      params.pageSize
-    }`,
+    `${devURL}/user/userList?page=${params.page}&pageSize=${params.pageSize}`,
     {
       method: "get",
       headers: {
@@ -41,19 +40,16 @@ export const getUserList = async params => {
 //   return res;
 // };
 export const saveUserFun = async params => {
-  const res = await fetch(
-    `${devURL}/user`,
-    {
-      method: "post",
-      headers: {
-        "Content-Type": 'application/json'
-      },
-      body: JSON.stringify(params)
-    }
-  )
-  .then(res => res.json())
-  .catch(err => {
-    throw err;
-  });
+  const res = await fetch(`${devURL}/user`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(params)
+  })
+    .then(res => res.json())
+    .catch(err => {
+      throw err;
+    });
   return res;
-}
+};
--- express_html/src/components/userList/index.js
+++ express_html/src/components/userList/index.js
@@ -1,11 +1,27 @@
+/* eslint-disable react/display-name */
 /* eslint-disable react/prop-types */
 import React, { Component } from "react";
 import { withRouter } from "next/router";
-import { Table , Modal , Button , Form ,Input ,message , Card} from "antd";
+import {
+  Table,
+  Modal,
+  Button,
+  Form,
+  Input,
+  message,
+  Icon,
+  Row,
+  Col,
+  Select,
+  InputNumber,
+  DatePicker
+} from "antd";
+import moment from "moment";
 
-import './index.scss';
+import "./index.scss";
 
 const FormItem = Form.Item;
+const { Option } = Select;
 const columns = [
   {
     title: "用户名",
@@ -25,11 +41,17 @@ const columns = [
   {
     title: "创建日期",
     sorter: true,
-    dataIndex: "createdAt"
+    dataIndex: "createdAt",
+    render: val => {
+      return <span>{moment(val).format("YYYY-MM-DD HH:mm:ss")}</span>;
+    }
   },
   {
     title: "修改日期",
-    dataIndex: "updatedAt"
+    dataIndex: "updatedAt",
+    render: val => {
+      return <span>{moment(val).format("YYYY-MM-DD HH:mm:ss")}</span>;
+    }
   }
 ];
 const CreateForm = Form.create()(props => {
@@ -50,47 +72,49 @@ const CreateForm = Form.create()(props => {
       onCancel={() => handleCreateVisible()}
     >
       <FormItem labelCol={{ span: 5 }} wrapperCol={{ span: 15 }} label="用户名">
-        {form.getFieldDecorator('username', {
-          rules: [{ required: true, message: '用户名最少两字符！', min: 2}],
+        {form.getFieldDecorator("username", {
+          rules: [{ required: true, message: "用户名最少两字符！", min: 2 }]
         })(<Input placeholder="请输入" />)}
       </FormItem>
     </Modal>
   );
 });
 
+@Form.create()
 class UserListPage extends Component {
   state = {
     pagination: {
       current: 1,
       pageSize: 5
     },
-    createFormVisible: false
-
+    createFormVisible: false,
+    expandForm: false
   };
   //新增按钮
-  handleCreateVisible = flag =>{
+  handleCreateVisible = flag => {
     this.setState({
       createFormVisible: !!flag
-    })
-  }
+    });
+  };
+  //弹框的添加按钮
   handleAdd = fields => {
-    let params= Object.assign(fields,{picture:'baidu.com'})
+    let params = Object.assign(fields, { picture: "baidu.com" });
     this.props.saveUserList(params);
-    const pagination = {...this.state.pagination};
-    pagination.current =1;
+    const pagination = { ...this.state.pagination };
+    pagination.current = 1;
     this.setState({
       pagination
     });
     const pageParams = {
-      page:1,
-      pageSize:this.state.pagination.pageSize
-    }
+      page: 1,
+      pageSize: this.state.pagination.pageSize
+    };
     this.props.getUserList(pageParams);
 
-    message.success('添加成功');
+    message.success("添加成功");
     this.handleCreateVisible();
   };
-
+  //点击分页
   handleTableChange = pagination => {
     const pager = { ...this.state.pagination };
     pager.current = pagination.current;
@@ -103,6 +127,43 @@ class UserListPage extends Component {
     };
     this.props.getUserList(params);
   };
+  //搜索方法
+  handleFormReset = () => {
+    const { form, dispatch } = this.props;
+    form.resetFields();
+    this.setState({
+      formValues: {}
+    });
+  };
+  toggleForm = () => {
+    const { expandForm } = this.state;
+    this.setState({
+      expandForm: !expandForm
+    });
+  };
+  handleSearch = e => {
+    e.preventDefault();
+
+    const { dispatch, form } = this.props;
+
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+
+      const values = {
+        ...fieldsValue,
+        updatedAt: fieldsValue.updatedAt && fieldsValue.updatedAt.valueOf()
+      };
+
+      this.setState({
+        formValues: values
+      });
+
+      // dispatch({
+      //   type: "rule/fetch",
+      //   payload: values
+      // });
+    });
+  };
 
   componentDidMount() {
     const params = {
@@ -112,21 +173,124 @@ class UserListPage extends Component {
     this.props.getUserList(params);
   }
 
+  //展开和收起
+  renderSimpleForm() {
+    const {
+      form: { getFieldDecorator }
+    } = this.props;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="用户名">
+              {getFieldDecorator("username")(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="邮箱">
+              {getFieldDecorator("email")(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <span className="submitButtons">
+              <Button type="primary" htmlType="submit">
+                查询
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+                重置
+              </Button>
+              <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+                展开 <Icon type="down" />
+              </a>
+            </span>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  renderAdvancedForm() {
+    const {
+      form: { getFieldDecorator }
+    } = this.props;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="用户名">
+              {getFieldDecorator("username")(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="邮箱">
+              {getFieldDecorator("email")(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="创建日期">
+              {getFieldDecorator("createdAt")(
+                <DatePicker
+                  style={{ width: "100%" }}
+                  placeholder="请输入创建日期"
+                />
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="修改日期">
+              {getFieldDecorator("updatedAt")(
+                <DatePicker
+                  style={{ width: "100%" }}
+                  placeholder="请输入修改日期"
+                />
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <div style={{ overflow: "hidden" }}>
+          <div style={{ marginBottom: 24 }}>
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+            <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+              重置
+            </Button>
+            <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+              收起 <Icon type="up" />
+            </a>
+          </div>
+        </div>
+      </Form>
+    );
+  }
+
+  renderForm = () => {
+    const { expandForm } = this.state;
+    return expandForm ? this.renderAdvancedForm() : this.renderSimpleForm();
+  };
+
   render() {
     let pagination = this.state.pagination;
     pagination.total = this.props.userListTab.total;
 
     const data = this.props.userListTab.users,
       loading = this.props.loading;
-    const {createFormVisible} = this.state;
-    const createFMethods ={
-      handleAdd:this.handleAdd,
-      handleCreateVisible: this.handleCreateVisible,
-    }
+    const { createFormVisible } = this.state;
+    const createFMethods = {
+      handleAdd: this.handleAdd,
+      handleCreateVisible: this.handleCreateVisible
+    };
     return (
       <div>
-        <div className ='searchTopBox'>
-          <Button type="primary" icon="plus" onClick={() => this.handleCreateVisible(true)}>
+        <div className="searchTopBox">
+          <div className="tableListForm">{this.renderForm()}</div>
+          <Button
+            type="primary"
+            icon="plus"
+            onClick={() => this.handleCreateVisible(true)}
+          >
             新建
           </Button>
         </div>
@@ -139,7 +303,7 @@ class UserListPage extends Component {
           onChange={this.handleTableChange}
         />
 
-      <CreateForm {...createFMethods} createFormVisible={createFormVisible} />
+        <CreateForm {...createFMethods} createFormVisible={createFormVisible} />
       </div>
     );
   }
